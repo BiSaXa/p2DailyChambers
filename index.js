b212def1aa2a4f555215279f7bcb7475f86f2459
Arda Serdar Pektezol <info@ardapektezol.com>
2021-06-23 01:13:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.on('ready', () => {
   console.log('yeet')
 })
 
-const daily = new cron.CronJob('13 * * * * *', async(msg) => {
+const daily = new cron.CronJob('14 * * * * *', async(msg) => {
   console.log('sending reminder')
   const pbchannel = await client.channels.cache.find(channel => channel.id === '856992133888868392')
   const reminder = ("It\'s " + weekNames[d.getDay()-1] + " " + monthNames[d.getMonth()+1] + " " + dateOrdinal(d.getDate()) +
