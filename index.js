d784124df819c0856d83ee99be56d39a4f4404db
Arda Serdar Pektezol <info@ardapektezol.com>
2021-06-23 00:08:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var d = new Date();
      return d+(31==d||21==d||1==d?"st":22==d||2==d?"nd":23==d||3==d?"rd":"th")
  };
 
- const reminder = ("It\'s" + monthNames[d.getMonth()+1] + ' ' + dateOrdinal(d.getDate()))
+ const reminder = ("It\'s " + monthNames[d.getMonth()+1] + ' ' + dateOrdinal(d.getDate()) + date.getHours() )
 
 client.on('ready', async(msg) => {
   console.log('yeet')
