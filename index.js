ed75c0735cdddeac221ececcf381a4555bf96ff4
Arda Serdar Pektezol <info@ardapektezol.com>
2021-06-22 23:42:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ client.on('ready', () => {
 client.on('message', async(msg) => {
   const pbchannel = await client.channels.cache.find(channel => channel.id === 856992133888868392)
   pbchannel.send(new Discord.MessageEmbed().setColor("#FFFFFF").setAuthor("BSX").setDescription('bruh'))
+  .catch(err => console.log(err))
 })
 
 client.login(process.env.BOT_TOKEN)
