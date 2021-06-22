a9d516e5e8d2d03d3a4d0df9f5bf9c1fdcd373f1
Arda Serdar Pektezol <info@ardapektezol.com>
2021-06-22 23:45:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
-client.on('ready', () => {
+client.on('ready', async(msg) => {
   console.log('yeet')
-})
-
-client.on('message', async(msg) => {
   const pbchannel = await client.channels.cache.find(channel => channel.id === '856992133888868392')
   pbchannel.send(new Discord.MessageEmbed().setColor("#FFFFFF").setAuthor("BSX").setDescription('bruh'))
   .catch(err => console.log(err))
+})
+
+client.on('message', async(msg) => {
+
 })
 
 client.login(process.env.BOT_TOKEN)
