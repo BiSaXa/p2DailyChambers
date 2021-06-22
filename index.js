3a792383b071e661f0a2aec9d0e15f3bb9d2cfef
Arda Serdar Pektezol <info@ardapektezol.com>
2021-06-22 23:21:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 
 client.on('ready', () => {
+  const pb-posting = client.channels.cache.find(channel => channel.id === 856992133888868392)
   console.log('yeet')
+  pb-posting.send('Todays chamber is death.')
 })
 
 client.login(process.env.BOT_TOKEN)
