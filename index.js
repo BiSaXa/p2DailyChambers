277247ab41dfc5879399b0418438565817e124ef
Arda Serdar Pektezol <info@ardapektezol.com>
2021-06-23 00:13:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const monthNames = ["January", "February", "March", "April", "May", "June",
  "July", "August", "September", "October", "November", "December"]
+const weekNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 var d = new Date();
 
  function dateOrdinal(d) {
      return d+(31==d||21==d||1==d?"st":22==d||2==d?"nd":23==d||3==d?"rd":"th")
  };
 
- const reminder = ("It\'s " + monthNames[d.getMonth()+1] + ' ' + dateOrdinal(d.getDate()) + d.getHours() )
+ const reminder = ("It\'s " + weekNames[d.getDay()+1] + monthNames[d.getMonth()+1] + ' ' + dateOrdinal(d.getDate()) )
 
 client.on('ready', async(msg) => {
   console.log('yeet')
