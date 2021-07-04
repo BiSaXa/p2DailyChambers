dbc11771e5fb9dfeca0f3d00e5241d85b6ec57ef
Arda Serdar Pektezol <info@ardapektezol.com>
2021-07-04 19:24:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ function randomNumber(min, max) {
 
 client.on('ready', () => {
   const guild = client.guilds.cache.get('146404426746167296')
+  guild.fetchMembers()
   const roleamount = guild.roles.cache.find(role => role.name === "Daily Chambers")
   console.log('yeet')
   console.log(roleamount.members.size)
